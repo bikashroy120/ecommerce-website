ae7c959b1810620700382999b44b413684ae3234
bikashroy120 <bikashroydt@gmail.com>
2023-04-10 21:19:06 +0600

    axios redux add

---
+++ src/services/auth/authServices.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+// import { config } from "../../utils/axiosconfig";
+import { base_url } from "../../utils/baseUrl";
+
+
+const regester = async(userData)=>{
+    const responces = await axios.post(`${base_url}/user/regester`,userData)
+    return responces.data
+}
+
+const login = async(userData)=>{
+    const responces = await axios.post(`${base_url}/user/regester`,userData)
+    return responces.data
+}
+
+
+const authService = {
+    login,
+    regester
+  };
+  
+  export default authService;
---
+++ src/services/auth/authSlice.js
@@ -0,0 +1,56 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+
+const getUserfromLocalStorage = localStorage.getItem("user")
+  ? JSON.parse(localStorage.getItem("user"))
+  : null;
+const initialState = {
+  user: getUserfromLocalStorage,
+  orders: [],
+  isError: false,
+  isLoading: false,
+  isSuccess: false,
+  message: "",
+};
+
+
+
+export const login = createAsyncThunk(
+    "auth/login",
+    async (userData, thunkAPI) => {
+      try {
+        return;
+      } catch (error) {
+        return thunkAPI.rejectWithValue(error);
+      }
+    }
+  );
+
+
+
+export const authSlice = createSlice({
+    name: "auth",
+    initialState: initialState,
+    reducers: {},
+    extraReducers: (buildeer) => {
+        buildeer
+        .addCase(login.pending, (state) => {
+          state.isLoading = true;
+        })
+        .addCase(login.fulfilled, (state, action) => {
+          state.isError = false;
+          state.isLoading = false;
+          state.isSuccess = true;
+          state.user = action.payload;
+          state.message = "success";
+        })
+        .addCase(login.rejected, (state, action) => {
+          state.isError = true;
+          state.isSuccess = false;
+          state.message = action.error;
+          state.isLoading = false;
+        })
+    }
+
+
+})
